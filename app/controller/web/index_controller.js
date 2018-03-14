8478767dba143234eec644b957b64a486d131cee
dotmaik1 <dotmaik1@gmail.com>
2018-03-13 19:08:48 -0700

    Se agregaron las rutas de la administracion

--- app/controller/web/index_controller.js
+++ app/controller/web/index_controller.js
@@ -1,10 +1,12 @@
 let Router = require('express').Router;
 let userRouter = require('./user_controller').router;
 let dispatcherRouter = require('./dispatcher_controller').router;
+let administrativeRouter = require('./administrative_controller').router;
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/dispatcher', dispatcherRouter);
+router.use('/administrative', administrativeRouter);
 
 exports.router = router;
