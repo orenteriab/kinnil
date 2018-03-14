3c69b3b6bba7f99a28c3e8cba4c0ca26f3a6b61d
dotmaik1 <dotmaik1@gmail.com>
2018-03-13 19:09:18 -0700

    controller added

---
+++ app/controller/api/administrative_controller.js
@@ -0,0 +1,14 @@
+let Router = require('express').Router;
+let administrativeService = require('../../service/administrative_service');
+
+const ROUTER = Router();
+
+/*
+* Endpoint -> driversUp, trucksUps and trailersUp
+*/
+ROUTER.get('/', (req, res) => {
+
+    
+});
+
+exports.router = ROUTER;
