9033a6f5816bbbfb3196873d3348dbb9079b9ff8
dotmaik1 <dotmaik1@gmail.com>
2018-03-13 19:09:44 -0700

    Se agrego la ruta para administrative

--- app/controller/api/index_controller.js
+++ app/controller/api/index_controller.js
@@ -1,9 +1,11 @@
 let Router = require('express').Router;
 let usersRouter = require('./user_controller').router;
 let dispatcherRouter = require('./dispatcher_controller').router;
+let administrativeRouter = require('./administrative_controller').router;
 
 const router = Router();
 router.use('/users', usersRouter);
 router.use('/dispatcher', dispatcherRouter);
+router.use('/administrative', administrativeRouter);
 
 exports.router = router;
