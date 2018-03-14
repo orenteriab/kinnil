27449505c6d9d158c8da309d78e0f86e1cd07024
Efrain Renteria <erenteria@1aauto.com>
2018-03-14 16:25:52 -0700

    Completado fase para subir csv, se hace via AJAX para no tener que fastidiar al disco duro.

--- app/controller/api/index_controller.js
+++ app/controller/api/index_controller.js
@@ -1,9 +1,11 @@
 let Router = require('express').Router;
 let usersRouter = require('./user_controller').router;
 let dispatcherRouter = require('./dispatcher_controller').router;
+let ticketRouter = require('./tickets_controller').router;
 
 const router = Router();
 router.use('/users', usersRouter);
 router.use('/dispatcher', dispatcherRouter);
+router.use('/tickets', ticketRouter);
 
 exports.router = router;
--- app/controller/api/tickets_controller.js
+++ app/controller/api/tickets_controller.js
@@ -1,15 +1,14 @@
 const ROUTER = require('express').Router();
-const MULTER = require('multer');
-const STORAGE = MULTER.memoryStorage();
-const UPLOAD_HANDLER = MULTER({ storage: STORAGE });
+//const MULTER = require('multer');
+//const STORAGE = MULTER.memoryStorage();
+//const UPLOAD_HANDLER = MULTER({ storage: STORAGE });
 
-ROUTER.post('/upload-csv',
-    UPLOAD_HANDLER.single('csv_file'),
-    (req, res) => {
-        //req.file contains all the file information
-        //we just need a way to dump it. Adding csv to json
-        //later.
-        res.send('file uploaded');
-    });
+ROUTER.post('/upload/csv', (req, res) => {
+    //req.file contains all the file information
+    //we just need a way to dump it. Adding csv to json
+    //later.
+    console.log(req.body.file);
+    res.send(JSON.stringify({ message: 'Uploaded!' }));
+});
 
 exports.router = ROUTER;
