62eaa2e4bf3f1bf2be80bd52ba42ab93ec76003b
Efrain Renteria <erenteria@1aauto.com>
2018-03-15 15:56:34 -0700

    Just receiving the csv record

--- app/controller/api/tickets_controller.js
+++ app/controller/api/tickets_controller.js
@@ -4,11 +4,7 @@ const ROUTER = require('express').Router();
 //const UPLOAD_HANDLER = MULTER({ storage: STORAGE });
 
 ROUTER.post('/upload/csv', (req, res) => {
-    //req.file contains all the file information
-    //we just need a way to dump it. Adding csv to json
-    //later.
-    console.log(req.body.file);
-    res.send(JSON.stringify({ message: 'Uploaded!' }));
+    res.json({ message: `${req.body.record['TMS Load #']} received!` });
 });
 
 exports.router = ROUTER;
