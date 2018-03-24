30baf89ce87e658c30d5ebd93279b6f0dacaf547
Efrain Renteria <erenteria@1aauto.com>
2018-03-23 18:03:19 -0700

    login controller :D

---
+++ app/controller/web/login_controller.js
@@ -0,0 +1,40 @@
+const Router = require('express').Router;
+const router = Router();
+const passport = require('../../config/passport_config');
+
+router.get('/', (req, res) => {
+    let reqFlash = {};
+    
+    if(req.flash){
+        reqFlash = req.flash();
+    }
+
+    res.render('pages/login.ejs', { message: reqFlash.message || '' });
+});
+
+router.post('/', passport.loginAuthenticator, (req, res) => {
+    req.session.save((err) => {
+        if(err){
+            res.render('pages/login.ejs', { message: JSON.stringify(err) });
+        }
+
+        res.redirect('/web/dispatcher/inicio');
+    });
+});
+
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    req.session.save((err) => {
+        if(err){
+            return next(err);
+        }
+
+        res.redirect('/web/login');
+    });
+});
+
+router.get('/signup', (req, res) => res.render('pages/signup.ejs', { message: '' }));
+
+router.post('/signup', passport.signUpAuthenticator);
+
+module.exports.router = router;
