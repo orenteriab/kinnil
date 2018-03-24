398948360c0ef7a5e9c1b6de26318b95644e3c7c
Efrain Renteria <erenteria@1aauto.com>
2018-03-23 18:03:08 -0700

    agregando verificación a todo

--- app/controller/web/index_controller.js
+++ app/controller/web/index_controller.js
@@ -2,11 +2,14 @@ let Router = require('express').Router;
 let userRouter = require('./user_controller').router;
 let dispatcherRouter = require('./dispatcher_controller').router;
 let administrativeRouter = require('./administrative_controller').router;
+let loginRouter = require('./login_controller').router;
+let passportVerification = require('../../config/passport_config').isLoggedIn;
 
 const router = Router();
 
-router.use('/users', userRouter);
-router.use('/dispatcher', dispatcherRouter);
-router.use('/administrative', administrativeRouter);
+router.use('/login', loginRouter);
+router.use('/users', passportVerification, userRouter);
+router.use('/dispatcher', passportVerification, dispatcherRouter);
+router.use('/administrative', passportVerification, administrativeRouter);
 
 exports.router = router;
