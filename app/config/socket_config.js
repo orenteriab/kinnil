b65081061f38ae499d1b0d870ca3bb6fcef55124
Efrain Renteria <erenteria@1aauto.com>
2018-04-02 12:46:51 -0600

    sockets

--- app/config/socket_config.js
+++ app/config/socket_config.js
@@ -8,7 +8,7 @@ const DISPATCHER_EVENTS = require('../socket/dispatcher_events').onConnection;
 //mensaje "/dispatcher/message" va a existir la respuesta a ese evento
 //y que de ahí, nadie más lo va a tomar.
 // Ya que por ejemplo, ahorita, el evento "message" nadie más lo va a
-// poder usar.
+// poder usar o se va a escuchar "de más".
 exports.init = (server) => {
     const IO = SOCKET_BROKER(server);
     IO.on(DISPATCHER_EVENTS.name, DISPATCHER_EVENTS.handler);
