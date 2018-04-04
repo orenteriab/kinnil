b5d172ed0fcad219bf7fcc01c032afb936531569
Efrain Renteria <erenteria@1aauto.com>
2018-04-04 16:10:44 -0600

    adding asset from controller

--- app/controller/web/assets_controller.js
+++ app/controller/web/assets_controller.js
@@ -1,8 +1,14 @@
 let Router = require('express').Router
 let router = Router();
+let assetsService = require('../../service/assets_service');
 
 router.get('/', (req, res) => {
-    res.render('pages/assets.ejs', { message: '', tickets: {}, drivers: {}, products: {} })
+    assetsService.pullPageAsset().then((value) => {
+        res.render('pages/assets.ejs', { message: '', assets: value })
+    }, (error) => {
+        console.error(error);
+        res.sendStatus(500);
+    });
 });
 
 exports.router = router;
