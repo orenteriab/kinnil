2ee7fb86911c88717af5e029d844b9f0adc4fde4
Efrain Renteria <erenteria@1aauto.com>
2018-04-04 16:11:15 -0600

    service for delete and pull assets

--- app/service/assets_service.js
+++ app/service/assets_service.js
@@ -1,3 +1,4 @@
+let isNumber = require('util').isNumber;
 let model = require('../model/assets_model')
 
 exports.create = (name, type, plate, status, mi, miLastService, mttoLast, mttoNext, notes, clientsId) => {
@@ -11,3 +12,15 @@ exports.update = (id, name, type, plate, status, mi, miLastService, mttoLast, mt
 exports.findById = (id) => {
     return model.findAssetById(id);
 }
+
+exports.deleteAsset = (id) => {
+    return model.deleteAsset(id);
+};
+
+exports.pullPageAsset = (page, size) => {
+    if(!page || !isNumber(page)) page = 1;
+    if(!size || !isNumber(size)) size = 10;
+    let offset = (page - 1) * size;
+
+    return model.pullPageAsset(offset, size);
+} 
