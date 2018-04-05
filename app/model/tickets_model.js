68eef18be53250bacee73668ef437faefb194bf0
dotmaik1 <dotmaik1@gmail.com>
2018-04-05 16:21:09 -0600

    Ajustes para recibir el nuevo csv

--- app/model/tickets_model.js
+++ app/model/tickets_model.js
@@ -2,37 +2,23 @@ let connectionPool = require('../config/database_config').connectionPool;
 let moment = require('moment-timezone');
 
 exports.create = (ticket) => {
-    let query = "INSERT INTO `sandras`.`tickets`(`tms`,`status`,`substatus`,`price`,`currency`,`product`,`base`,`silo`,`po`,`facility`,`location`,`bol`,`sand_type`,`weight`,`assign_date`,`completed_date`,`invoice_date`,`payrolled_date`,`starting_mi`,`end_mi`,`pick_date`,`drop_date`,`pro`,`equipment`,`notes`,`sr`,`hr_id`,`products_id`,`clients_id`) VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,? )";
+    let query = "INSERT INTO `sandras`.`tickets`(`tms`,`status`,`substatus`,`price`,`currency`,`load_rate`,`load_rate_currency`,`product`,`facility`,`location`,`sand_type`,`pick_date`,`drop_date`,`products_id`,`clients_id`) VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
     return connectionPool.query(query, [
         ticket["TMS Load #"],
-        ticket["Status"],
+        1, // El primer status siempre es 1 no importa lo que venga en el cvs
         0, //substatus
-        ticket["Rate"],
+        ticket["Rate Invoice"],
         ticket["Currency"],
-        '', //product
+        ticket["Load Rate"],
+        ticket["Currency"],
+        ticket["Miles"], //product
         ticket["Origin"],
-        '',//silo
-        ticket["Order #"],
         ticket["Destination"],
-        ticket["Origin"],
-        ticket["BOL #"],
-        '', //sand_type
-        ticket["Load Weight (lb)"],
-        ticket["Accept Date"],
-        ticket["Drop Date"],
-        ticket["Tender Date"],
-        ticket["Close Date"],
-        0,//starting_mi,
-        0,//end_mi
+        ticket["Sand Type"],
         ticket["Pick Date"],
         ticket["Drop Date"],
-        ticket["Pro#"],
-        ticket["Equipment"],
-        ticket["Load Notes"],
-        ticket["Service Requests"],
-        1,//hr_id,
-        1,//products_id,
-        1//ticket["Primary Reference #"]
+        1, // TODO: Esto va a cambiar en la 2nda o 3ra etapas 
+        1 // 1 porque siempre en esta etapa es halliburton
     ]);
 };
