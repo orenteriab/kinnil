03e51cab45dc6755a4ac7c52969918bbf350d848
Efrain Renteria <erenteria@1aauto.com>
2018-04-06 17:48:18 -0600

    linting

--- app/model/tickets_model.js
+++ app/model/tickets_model.js
@@ -1,23 +1,22 @@
 let connectionPool = require('../config/database_config').connectionPool;
-let moment = require('moment-timezone');
 
 exports.create = (ticket) => {
-    let query = "INSERT INTO `sandras`.`tickets`(`tms`,`status`,`substatus`,`price`,`currency`,`load_rate`,`load_rate_currency`,`product`,`facility`,`location`,`sand_type`,`pick_date`,`drop_date`,`products_id`,`clients_id`) VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    let query = 'INSERT INTO `sandras`.`tickets`(`tms`,`status`,`substatus`,`price`,`currency`,`load_rate`,`load_rate_currency`,`product`,`facility`,`location`,`sand_type`,`pick_date`,`drop_date`,`products_id`,`clients_id`) VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
 
     return connectionPool.query(query, [
-        ticket["TMS Load #"],
+        ticket['TMS Load #'],
         1, // El primer status siempre es 1 no importa lo que venga en el cvs
         0, //substatus
-        ticket["Rate Invoice"],
-        ticket["Currency"],
-        ticket["Load Rate"],
-        ticket["Currency"],
-        ticket["Miles"], //product
-        ticket["Origin"],
-        ticket["Destination"],
-        ticket["Sand Type"],
-        ticket["Pick Date"],
-        ticket["Drop Date"],
+        ticket['Rate Invoice'],
+        ticket['Currency'],
+        ticket['Load Rate'],
+        ticket['Currency'],
+        ticket['Miles'], //product
+        ticket['Origin'],
+        ticket['Destination'],
+        ticket['Sand Type'],
+        ticket['Pick Date'],
+        ticket['Drop Date'],
         1, // TODO: Esto va a cambiar en la 2nda o 3ra etapas 
         1 // 1 porque siempre en esta etapa es halliburton
     ]);
