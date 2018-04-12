78a8ee16748e3adf9eac67857b4f34fb1a27c3c3
Efrain Renteria <erenteria@1aauto.com>
2018-04-12 12:52:07 -0600

    up al crear

--- app/service/assets_service.js
+++ app/service/assets_service.js
@@ -1,8 +1,8 @@
 let isNumber = require('util').isNumber;
 let model = require('../model/assets_model')
 
-exports.create = (name, type, plate, status, mi, miLastService, mttoLast, mttoNext, notes, clientsId) => {
-    return model.createAsset(name, type, plate, status, mi, miLastService, mttoLast, mttoNext, notes, clientsId);
+exports.create = (name, type, plate, status, mi, miLastService, mttoLast, mttoNext, notes, clientsId, up) => {
+    return model.createAsset(name, type, plate, status, mi, miLastService, mttoLast, mttoNext, notes, clientsId, up);
 }
 
 exports.update = (id, name, type, plate, status, mi, miLastService, mttoLast, mttoNext, notes, clientsId, up) => {
