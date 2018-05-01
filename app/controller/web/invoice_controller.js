a2580b542358b8c534503676b71a51d9068a51ef
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2018-05-01 13:12:45 -0600

    home de invoice

--- app/controller/web/invoice_controller.js
+++ app/controller/web/invoice_controller.js
@@ -3,7 +3,9 @@ let invoiceService = require('../../service/invoice_service');
 
 const router = Router();
 
-
+router.get('/home', (req, res) => {
+    res.render('pages/invoice.ejs')
+})
 
 
 exports.router = router;
