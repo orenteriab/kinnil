8534df34f88b849d4a0331747d1b81c5c30a5151
dotmaik1 <dotmaik1@gmail.com>
2018-09-01 14:57:51 -0600

    Tickets updaload detail

--- app/controller/api/tickets_controller.js
+++ app/controller/api/tickets_controller.js
@@ -6,12 +6,12 @@ ROUTER.post('/upload/csv', (req, res) => {
         .create(req.body.record)
         .then(() => {
             res.status(201);
-            res.json({ message: `${req.body.record['TMS Load #']} received!` });
+            res.json(`${req.body.record['TMS Load #']} received!`);
         })
         .catch((err) => {
             res.status(400);
             console.log(err)
-            res.json({ message: `${req.body.record['TMS Load #']} was impossible to store. Please verify its information is correct.` });
+            res.json(`${req.body.record['TMS Load #']} was impossible to store. Please verify its information is correct.`);
         });
 });
 
