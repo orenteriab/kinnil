8a2fbc00e1f3e789d95a7d981666821e1701478c
dotmaik1 <dotmaik1@gmail.com>
2018-10-15 19:13:23 -0600

    Quickbooks report

--- app/model/reports_model.js
+++ app/model/reports_model.js
@@ -18,3 +18,25 @@ where born_date >= ? and born_date <= ?'
     return connectionPool.query(statement,[inicio, fin]);
 };
 
+exports.getQuickbooksReport = (inicio, fin) => {
+    let statement = "select t.ticket_id, \
+DATE_FORMAT(`t`.`completed_date`, '%m-%d-%Y') completed_date, \
+t.tms, \
+t.bol, \
+t.weight, \
+t.sand_type, \
+t.product, \
+d.name driver_name, \
+l.name location_name, \
+l.long_name location_long_name, \
+f.long_name facility_long_name, \
+ROUND(t.invoice_rate - t.load_rate, 2) fuel_surcharge \
+from tickets t \
+left join hr d on t.hr_id = d.id \
+left join locations l on l.name = t.location \
+left join facilities f on f.name = t.facility \
+where completed_date >= ? and completed_date <= ? \
+order by l.name"
+
+    return connectionPool.query(statement,[inicio, fin]);
+};
