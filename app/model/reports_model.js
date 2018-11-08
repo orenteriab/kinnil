2f0568e9834dd057fbbca769472562e57e73c10c
dotmaik1 <dotmaik1@gmail.com>
2018-11-07 18:09:02 -0700

    Fix para el reporte de invoice

--- app/model/reports_model.js
+++ app/model/reports_model.js
@@ -19,23 +19,23 @@ where born_date >= ? and born_date <= ?'
 };
 
 exports.getQuickbooksReport = (inicio, fin) => {
-    let statement = "select t.ticket_id, \
-DATE_FORMAT(`t`.`completed_date`, '%m-%d-%Y') completed_date, \
+    let statement = "select IFNULL(t.ticket_id, 'Not provided yet') ticket_id, \
+DATE_FORMAT(`t`.`loading_date`, '%m/%d/%Y') completed_date, \
 t.tms, \
 t.bol, \
 t.weight, \
 t.sand_type, \
 t.product, \
 d.name driver_name, \
-l.name location_name, \
-l.long_name location_long_name, \
-f.long_name facility_long_name, \
-ROUND(t.invoice_rate - t.load_rate, 2) fuel_surcharge \
+IFNULL(l.name, 'Wrong data input') location_name, \
+IFNULL(l.long_name, 'Wrong data input') location_long_name, \
+IFNULL(f.long_name, 'Wrong data input') facility_long_name, \
+ROUND(t.invoice_rate - t.fixed_rate, 2) fuel_surcharge \
 from tickets t \
 left join hr d on t.hr_id = d.id \
 left join locations l on l.name = t.location \
 left join facilities f on f.name = t.facility \
-where completed_date >= ? and completed_date <= ? \
+where DATE(t.loading_date) >= ? and DATE(t.loading_date) <= ? and t.status = 4 \
 order by l.name"
 
     return connectionPool.query(statement,[inicio, fin]);
