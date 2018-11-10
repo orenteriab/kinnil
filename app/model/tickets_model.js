857232593530bb13f0ecfe39b04ee188e541a7ef
Omar Efrain Renteria Balderrama <orenteriab@gmail.com>
2018-11-09 18:33:57 -0700

    Recibiendo exceles del nuevo cliente

--- app/model/tickets_model.js
+++ app/model/tickets_model.js
@@ -1,29 +1,49 @@
 let connectionPool = require('../config/database_config').connectionPool;
 let moment = require('moment-timezone');
+let Bluebird = require('bluebird');
 
 exports.create = (ticket) => {
-    let query = "INSERT INTO `sandras`.`tickets`(`tms`,`born_date` ,`status`,`substatus`,`invoice_rate`,`load_rate`, `fixed_rate`, `miles`,`product`,`driver_rate`,`facility`,`location`,`sand_type`,`pick_date`,`drop_date`, `po`, `products_id`,`clients_id`) VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    let shipper = ticket["Shipper"]
 
-    return connectionPool.query(query, [
-        ticket["TMS Load #"],
-        ticket["born_date"],
-        1, // El primer status siempre es 1 no importa lo que venga en el cvs
-        0, //substatus (accepted)
-        ticket["Rate Invoice"],
-        ticket["Load Rate"],
-        ticket["Fixed Rate"],
-        ticket["Actual Miles"],
-        ticket["Miles"], //product
-        ticket["Driver Rate"],
-        ticket["Origin"],
-        ticket["Destination"],
-        ticket["Sand Type"],
-        ticket["Pick Date"],
-        ticket["Drop Date"],
-        ticket["PO"],
-        1, // TODO: Esto va a cambiar en la 2nda o 3ra etapas 
-        1 // 1 porque siempre en esta etapa es halliburton
-    ]);
+    return connectionPool
+        .query('select  `id`                        \
+                from    `sandras`.`clients`         \
+                where   lower(`name`) = lower(?)    \
+                limit   1', [shipper])
+        .then(customerData => {
+            let customerId = customerData 
+                && customerData[0] 
+                && customerData[0].id ? customerData[0].id : -1;
+
+            if(customerId === -1){
+                return Bluebird.reject(`The shipper ${shipper} wasn't found.`);
+            }
+
+            let query = "INSERT INTO `sandras`.`tickets`(`tms`,`born_date` ,`status`,`substatus`,`invoice_rate`,`load_rate`, `fixed_rate`, `miles`,`product`,`driver_rate`,`facility`,`location`,`sand_type`,`pick_date`,`drop_date`, `po`, `products_id`,`clients_id`) VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            return connectionPool.query(query, [
+                ticket["TMS Load #"],
+                ticket["born_date"],
+                1, // El primer status siempre es 1 no importa lo que venga en el cvs
+                0, //substatus (accepted)
+                ticket["Rate Invoice"],
+                ticket["Load Rate"],
+                ticket["Fixed Rate"],
+                ticket["Actual Miles"],
+                ticket["Miles"], //product
+                ticket["Driver Rate"],
+                ticket["Origin"],
+                ticket["Destination"],
+                ticket["Sand Type"],
+                ticket["Pick Date"],
+                ticket["Drop Date"],
+                ticket["PO"],
+                1, // TODO: Esto va a cambiar en la 2nda o 3ra etapas 
+                customerId // 1 porque siempre en esta etapa es halliburton
+            ]);
+        })
+        .catch(err => {
+            return Bluebird.reject(`No shipper specified for ${ticket["TMS Load #"]}`)
+        })
 };
 
 exports.update = (ticket, id) => {
