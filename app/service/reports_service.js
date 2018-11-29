307efb6764ebc74f3432e9a2be7b38afeaa06c13
Omar Renteria <orenteriab@gmail.com>
2018-11-29 15:38:37 -0700

    Primero reviso por driver, luego cada driver tira una consulta y la promesa de esa consulta es agregada a un array, de ahí regreso la promesa una vez que todas han sido respondidas

--- app/service/reports_service.js
+++ app/service/reports_service.js
@@ -7,3 +7,17 @@ exports.getCompletedLoadsReport = (inicio, fin) => {
 exports.getQuickbooksReport = (inicio, fin, locationName) => {
     return model.getQuickbooksReport(inicio, fin, locationName)
 }
+
+exports.getDiamonBackReport = async(start, end) => {
+    try{
+        let drivers = await model.getDiamonbackDriversForReport(start, end)
+
+        let driverPromises = drivers.map((driver) => {
+            return model.getDiamonbackReportRecord(start, end, driver);
+        })
+
+        return Promise.all(driverPromises)
+    }catch(e){
+        return Promise.reject(e);
+    }
+}
