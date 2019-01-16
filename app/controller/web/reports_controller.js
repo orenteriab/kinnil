98324f494809774b4d9f1de888aef96cd4277709
dotmaik1 <dotmaik1@gmail.com>
2019-01-15 20:35:17 -0700

    reports update

--- app/controller/web/reports_controller.js
+++ app/controller/web/reports_controller.js
@@ -1,14 +1,21 @@
 let Router = require('express').Router;
 let reprotsService = require('../../service/reports_service');
+let verifyRole = require('../../roles/app_roles').verifyRole
 
 const router = Router();
 
-router.get('/home', (req, res) => {
+router.get('/home', verifyRole(['INVOICE ASSISTANT', 'INVOICE MANAGER', 'PAYROLL ASSISTANT HR']), (req, res) => {
 
     res.render('pages/reports.ejs', { 
         message: ''
     });
 });
 
+router.get('/search', verifyRole(['INVOICE ASSISTANT', 'INVOICE MANAGER', 'PAYROLL ASSISTANT HR', 'PAYROLL ASSISTANT DRIVERS']), (req, res) => {
+
+    res.render('pages/search.ejs', { 
+        message: ''
+    });
+});
 
 exports.router = router;
--- app/service/reports_service.js
+++ app/service/reports_service.js
@@ -1,4 +1,5 @@
 let model = require('../model/reports_model')
+let dateUtils = require('../helpers/date')
 
 exports.getCompletedLoadsReport = (inicio, fin) => {
     return model.getCompletedLoadsReport(inicio, fin)
@@ -9,15 +10,51 @@ exports.getQuickbooksReport = (inicio, fin, locationName) => {
 }
 
 exports.getDiamonBackReport = async(start, end) => {
-    try{
-        let drivers = await model.getDiamonbackDriversForReport(start, end)
-
+   try{
+       let drivers = await model.getDiamonbackDriversForReport(start, end)
         let driverPromises = drivers.map((driver) => {
-            return model.getDiamonbackReportRecord(start, end, driver);
-        })
-
+           return model.getDiamonbackReportRecord(start, end, driver);
+       })
         return Promise.all(driverPromises)
+   }catch(e){
+       return Promise.reject(e);
+   }
+}
+
+exports.getPayrollHrReport = async(start, end, position) => {
+    try{
+        let HRs = await model.getHrForReport(position)
+        let HRPromises = HRs.map((hr) => {
+            return model.getClockinInfoByHrId(start, end, hr.id);
+        })
+        return Promise.all(HRPromises)
     }catch(e){
         return Promise.reject(e);
     }
-}
+ }
+
+ exports.CEOReport = () => {
+
+    var currentDate = dateUtils.getCurrentDateAndTime()
+
+    let totalLoads = model.totalLoads(currentDate)
+    let totalVolume = model.totalVolume(currentDate)
+    let loadsPerClient = model.loadsPerClient(currentDate)
+    let sandsPerClient = model.sandsPerClient(currentDate)
+    let totalSandPerClient = model.totalSandPerClient(currentDate)
+    let clientsList = model.clientsList()
+
+    // Esperamos a que todas las promesas se cumplan para enviar la promesa final
+    var return_data = {};
+    return Promise.all([totalLoads,totalVolume,loadsPerClient,sandsPerClient,totalSandPerClient,clientsList]).then((data) => {
+        
+        return_data.totalLoads = data[0];
+        return_data.totalVolume = data[1];
+        return_data.loadsPerClient = data[2];
+        return_data.sandsPerClient = data[3];
+        return_data.totalSandPerClient = data[4];
+        return_data.clientsList = data[5];
+
+        return return_data;
+    });
+ }
